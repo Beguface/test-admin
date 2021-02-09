50c37ee867d35898b50513accf521190b3b14092
Cesar F <beguface@gmail.com>
2021-02-09 13:45:51 -0400

    main view layout added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
-import { Input } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Input } from "@chakra-ui/react";
 import Head from "next/head";
 
 export default function Home() {
-  return <Input placeholder="Hola" />;
+  return (
+    <>
+      <Head>
+        <title>Test Admin</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Center h="100vh">
+        <Box width="100%" px={{ base: "2%", sm: "5%", md: "20%" }}>
+          <Input placeholder="Ingrese el nombre del usuario" />
+          <Center>
+            <Button colorScheme="blue" type="submit" mt="4">
+              CREATE
+            </Button>
+          </Center>
+        </Box>
+      </Center>
+    </>
+  );
 }
