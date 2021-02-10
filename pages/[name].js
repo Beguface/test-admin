87b96d1656dec4cb2efb3111176662511d12e98d
Cesar F <beguface@gmail.com>
2021-02-10 15:37:43 -0400

    Trackable deep links added

--- pages/[name].js
+++ pages/[name].js
@@ -1,22 +1,56 @@
+import { useState } from "react";
 import { Button, Center, Heading, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import Error from "next/error";
+import firebaseDynamicLinks from "../utils/firebase";
+import SharedLink from "../components/SharedLink/SharedLink";
 
 const userPage = ({ user, statusCode }) => {
+  const [link, setLink] = useState({
+    shortLink: null,
+    isLoading: false,
+  });
+
+  const { shortLink, isLoading } = link;
+
   if (statusCode !== 200) {
     return <Error statusCode={statusCode} />;
   }
+
+  const handleGetLink = async () => {
+    setLink({ ...link, isLoading: true });
+    const { shortLink } = await firebaseDynamicLinks.createLink({
+      dynamicLinkInfo: {
+        domainUriPrefix: "https://electricbirdcage.page.link",
+        link: `https://electricbirdcage.com/${user.name}`,
+        androidInfo: {
+          androidPackageName: "com.example.android",
+        },
+        iosInfo: {
+          iosBundleId: "com.example.ios",
+        },
+      },
+    });
+    console.log(shortLink);
+    setLink({ shortLink, isLoading: false });
+  };
 
   return (
     <Center h="100vh" flexDirection="column">
       <Text as="h2" fontSize="lg">{`This page is for: `}</Text>
       <Heading as="span">{user?.name}</Heading>
+      {shortLink && <SharedLink link={shortLink} />}
       <Center mt="4">
         <Link href="/" passHref>
           <Button mr="5">BACK</Button>
         </Link>
-
-        <Button colorScheme="blue">SHARE</Button>
+        <Button
+          colorScheme="blue"
+          onClick={handleGetLink}
+          isLoading={isLoading}
+        >
+          SHARE
+        </Button>
       </Center>
     </Center>
   );
@@ -26,6 +60,7 @@ export default userPage;
 
 export async function getServerSideProps({ query }) {
   let { name } = query;
+
   try {
     let req = await fetch(`${process.env.APP_URI}/api/users/${name}`);
     let { data } = await req.json();
